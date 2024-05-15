e41fc482a125347bff3d6305585edb8eb24d16de
Kacper Polak <41890821+xStrixU@users.noreply.github.com>
2024-05-15 17:33:05 +0200

    feat: setup prisma (#9)

---
+++ src/modules/api/common/database/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+
+import { env } from '@/common/lib/env';
+
+declare global {
+	// eslint-disable-next-line no-var
+	var prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.prisma ?? new PrismaClient();
+
+if (env.NODE_ENV !== 'production') {
+	global.prisma = prisma;
+}
